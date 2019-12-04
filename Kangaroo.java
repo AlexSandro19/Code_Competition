eec26f54114046b20bfee77ffdf631c13271cd7b
Jan <jan.bogoryja@gmail.com>
2019-12-04 10:05:43 +0100

    Base class setup

---
+++ Kangaroo.java
@@ -0,0 +1,10 @@
+public class Kangaroo{
+
+   public static void main(Stirng[] args)
+   }
+      int x1,x2;  // Kangaroo Location
+      int v1,v2;  // Kangaroo Velocity
+      
+      //if (x1 == x2) // kangaroos meet
+   }
+}
